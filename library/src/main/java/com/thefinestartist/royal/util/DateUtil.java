0b89a6ac9abfb98895b1e23627f002b054e29201
TheFinestArtist <contact@thefinestartist.com>
2015-08-06 01:38:14 +0900

    Update 0.82.0.3

--- library/src/main/java/com/thefinestartist/royal/util/DateUtil.java
+++ library/src/main/java/com/thefinestartist/royal/util/DateUtil.java
@@ -3,7 +3,6 @@ package com.thefinestartist.royal.util;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.TimeZone;
 
 /**
  * Created by TheFinestArtist on 8/6/15.
@@ -19,8 +18,6 @@ public class DateUtil {
     public static String getDateFormat(Date date) {
         if (date == null)
             return null;
-
-        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
         return dateFormat.format(date);
     }
 }
